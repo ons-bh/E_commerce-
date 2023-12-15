ab7604f9eb1e00b189d17c39d4b1bc35555c0f4c
ons <onsbenhassen7@gmail.com>
2023-12-15 23:31:06 +0100

    premier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from './authentification/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ecommerce';
+  constructor(public authService: AuthService) { }
+logout() {
+this.authService.doLogout()
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ProductsModule } from './products/products.module';
+import { AuthentificationModule } from'./authentification/authentification.module';
+import { AjoutcategorieComponent } from './Components/Categories/ajoutcategorie/ajoutcategorie.component';
+import { ShoppingCartModule } from './shopping-cart/shopping-cart.module';
+import { ScategoriesModule } from './scategories/scategories.module';
+import { CategoriesModule } from './categories/categories.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AjoutcategorieComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ProductsModule,
+    AuthentificationModule,
+    ShoppingCartModule,
+    ScategoriesModule,
+    CategoriesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
